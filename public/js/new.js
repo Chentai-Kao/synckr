6d1e48dccb2cd844fe9ca89c3dd28bdef39ad453
Jocelin <jocelin@stanford.edu>
2014-02-09 20:26:46 -0800

    friendlist page add

--- public/js/new.js
+++ public/js/new.js
@@ -1,5 +1,28 @@
-  $(document).ready(function() {
+  $(document).ready(function(){
+    
     $("#st-date").datepicker().on('changeDate', function(e){$("#st-date").datepicker('hide')});
     $("#en-date").datepicker().on('changeDate', function(e){$("#en-date").datepicker('hide')});
-  });
+
+
+    var width = $('#friendList').width();
+    $("#friendList").offset({left: width});
+
+    $('#invite-form input').click(function(e) {
+    	e.preventDefault();
+    	$('#friendList').animate({left: 0}, 200);
+    	$('#placeholder').animate({left: -width}, 200);});
+
+    $('#back-new').click(function(e){
+    	e.preventDefault();
+     	$('#friendList').animate({left: width}, 200);
+    	$('#placeholder').animate({left: 0}, 200);});   	
+    
+    $('#ok-new').click(function(e){
+    	e.preventDefault();
+     	$('#friendList').animate({left: width}, 200);
+    	$('#placeholder').animate({left: 0}, 200);});   	
+    
+});
+
+
 
