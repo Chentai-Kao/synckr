2bbb05cdb3ba0f164f323c57096332e8cf8c3880
Jocelin <jocelin@stanford.edu>
2014-02-11 19:02:55 -0800

    event list page update(cancel button)

---
+++ public/js/list.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("#nav-left").click(function(e){
+		$(".event-title").toggleClass("event-title-move");
+		//$(".event-info").toggleClass("event-info-move");
+		$(".arrow").toggleClass("arrow-move");
+		$(".cancel-icon").toggleClass("cancel-icon-move");
+
+	});
+
+
+
+});
