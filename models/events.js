f450382be489559431876ae3fad9ae8b7cf1820b
Cobra Kao <ru6xul6@gmail.com>
2014-02-11 16:52:51 -0800

    Change the schema of model.
    An example is given below:
    
    var data = {
      title:        'event 1',
      description:  'the first event',
      startDate:    '2014-01-02',
      endDate:      '2014-01-05',
      deadline:     '2014-01-01',
      duration:     '2',
      participants: [
        {
          name: 'John',
          facebookId: '234236367634',
          slot: [
            {
              startDate: '2014-01-03',
              startTime: '4',
              duration: '2'
            },
            ...
          ]
        },
        ...
      ]
    };
    
    Then post it with AJAX:
    
    $.post('/newevent', data);

--- models/events.js
+++ models/events.js
@@ -1,14 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var slotSchema = new Schema({
+  startDate: { type: String, required: true },
+  startTime: { type: String, required: true },
+  duration:  { type: String, required: true }
+});
+
+var participantSchema = new Schema({
+  name:       { type: String, required: true },
+  facebookId: { type: String, required: true },
+  slot:       [slotSchema]
+});
+
 var eventSchema = new Schema({
-    title: String,
-    description: String,
-    startDate: Date,
-    endDate: Date,
-    deadline: Date,
-    duration: String,
-    participants: [String]
+  title:        { type: String, trim: true, required: true },
+  description:  { type: String, trim: true },
+  startDate:    { type: String, required: true },
+  endDate:      { type: String, required: true },
+  deadline:     { type: String, required: true },
+  duration:     { type: String, required: true },
+  participants: { type: [participantSchema], required: true }
 });
 
 module.exports = mongoose.model('events', eventSchema);
