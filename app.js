801fe921370b833149c6f8dfd1b171147c3194a5
Cobra Kao <ru6xul6@gmail.com>
2014-02-13 22:21:16 -0800

    Complete updating slot.
    Example:
    var eventId: "52fac59f75a885a6259de12a";
    var facebookId = "2342344564564564";
    $.post('/updateslot', {
      eventId: eventId,
      facebookId: facebookId,
      slot: [ {
        startDate: "2013-12-30",
        startTime: "2014-01-02",
        duration: "2"
        } ] });

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/project/:name', project.viewProject);
 app.get('/getevent/:eventId', events.getEvent);
 app.get('/eventlist/:facebookId', events.eventList);
 app.post('/newevent', events.create);
-app.post('/addslot/:eventId/:facebookId', events.addSlot);
+app.post('/updateslot', events.updateSlot);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
